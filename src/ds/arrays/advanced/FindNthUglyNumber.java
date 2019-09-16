09fc3716bf0439148b62d8c5eef5f5033b7c6d2a
Sandeep <sandeep.apjjal@gmail.com>
2019-09-16 08:39:10 +0530

    Find nth ugly number

--- src/ds/arrays/advanced/FindNthUglyNumber.java
+++ src/ds/arrays/advanced/FindNthUglyNumber.java
@@ -3,6 +3,31 @@ package ds.arrays.advanced;
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+/*
+Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The sequence 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, … shows the first 11 ugly numbers. By convention, 1 is included. Write a program to find Nth Ugly Number.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. T testcases follow. For each testcase there is one line of input that contains the number N.
+
+Output:
+Print the Nth Ugly Number.
+
+Constraints:
+1 ≤ T ≤ 104
+1 ≤ N ≤ 104
+
+Example:
+Input:
+2
+10
+4
+Output:
+12
+4
+
+Note 1: Using list or set instead of array consumes more execution time due to internal colleciton methods
+Note 2: use "long" array instead of "int", to find nth elements on edges of int range
+ */
 public class FindNthUglyNumber
  {
 	public static void main (String[] args)
