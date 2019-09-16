d4f01a933821cef076a1eab292f2d7a00ab8f32a
Sandeep <sandeep.apjjal@gmail.com>
2019-09-16 08:39:59 +0530

    unoushed changes

--- src/ds/arrays/ArrayGreaterElementsOnRight.java
+++ src/ds/arrays/ArrayGreaterElementsOnRight.java
@@ -7,16 +7,18 @@ public class ArrayGreaterElementsOnRight {
 		List<Integer> list2 = new ArrayList<Integer>();
 		int[] array = new int[]{12,3,44,2,5,-10,34,22,9,10,19 };
 		int partition = 10;  // Move all greater elements on right, greater than 10
-		int lastPos = array.length - 1;
-		for (int i = 0; i < array.length; i++) {
-			if(array[i] > partition){
-				list.add(array[i]);
-			}else{
-				list2.add(array[i]);
-			}
-		}
-		list.addAll(list2);
-		System.out.println(list);
+		System.out.println("input: "+ Arrays.toString(array));
+//		int lastPos = array.length - 1;
+//		for (int i = 0; i < array.length; i++) {
+//			if(array[i] > partition){
+//				list.add(array[i]);
+//			}else{
+//				list2.add(array[i]);
+//			}
+//		}
+//		list.addAll(list2);
+//		System.out.println(list);
+		System.out.println(Arrays.toString(shiftGreaterOnRight(array, partition)));
 	}
 	
 	// Without spaces
@@ -24,21 +26,26 @@ public class ArrayGreaterElementsOnRight {
     {
         int lastIndex = a.length ;
         for(int i = 0; i< a.length || i >= lastIndex-1 ; i++){
-            if( i >= lastIndex-1)
+			System.out.println("i checking for  " + a[i]);
+
+			if( i >= lastIndex-1)
                 break;
-        	if(a[i] > item){
-        		for (int j = lastIndex -1; j>=0; j--){
+        	if(a[i] > item ){
+				System.out.println("i Need to replace " + a[i]);
+				for (int j = lastIndex -1; j>=i; j--){
         			if(a[j] <= item){
         				int temp = a[j];
         				a[j] = a[i];
         				a[i] = temp;
-        				lastIndex = j;	
-        System.out.println("state Array is " + Arrays.toString(a));
-        				
+        				lastIndex = j;
+						System.out.println("j Replace " +a[i] +" with " + a[j] +" | lastIndex:" +lastIndex);
+						System.out.println("j state Array is " + Arrays.toString(a));
         				break;
         			}
         		}
-        	}
+        	}else{
+				System.out.println("i Ignore for "+ a[i]);
+			}
         }
         return a;
     }	
--- src/ds/arrays/advanced/MaxPrefixSumForRange.java
+++ src/ds/arrays/advanced/MaxPrefixSumForRange.java
@@ -5,137 +5,89 @@ import java.util.Random;
 import java.util.Scanner;
 
 /*
-Reference: https://practice.geeksforgeeks.org/problems/kth-smallest-element/0
-Given an array arr[] and a number K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+https://practice.geeksforgeeks.org/problems/maximum-prefix-sum-for-a-given-range/0
 
-Expected Time Complexity: O(n)
+Given an array of n integers and q queries, each query having a range from l to r. Find the maximum prefix-sum for the range l to r.
+NOTE: Assume indexing from 0.
 
+Input: a[] = {-1, 2, 3, -5}
+       q = 2
+       l = 0, r = 3
+       l = 1, r = 3
+
+Output: 4
+        5
+
+Explanation:- The range (0, 3) in the 1st query has
+              [-1, 2, 3, -5], since it is prefix,
+              we have to start from -1. Hence, the
+              max prefix sum will be -1 + 2 + 3 = 4.
+              The range (1, 3) in the 1st query has
+              [2, 3, -5], since it is prefix, we
+              have to start from 2. Hence, the max
+              prefix sum will be 2 + 3 = 5.
 Input:
-The first line of input contains an integer T, denoting the number of testcases. Then T test cases follow. Each test case consists of three lines. First line of each testcase contains an integer N denoting size of the array. Second line contains N space separated integer denoting elements of the array. Third line of the test case contains an integer K.
+The first line consists of an integer T i.e number of test cases. The first line of each test case consists of two integers n and q.Then next line consists of n spaced integers. The last line of each test case consists of q pairs of integers representing l and r.
 
 Output:
-Corresponding to each test case, print the kth smallest element in a new line.
+Print the required answer.
 
 Constraints:
-1 <= T <= 100
-1 <= N <= 105
-1 <= arr[i] <= 105
-1 <= K <= N
+1<=T<=100
+1<=n,q,a[i]<=104
 
 Example:
 Input:
 2
-6
-7 10 4 3 20 15
-3
-5
-7 10 4 20 15
-4
+4 2
+-1 2 3 -5
+0 3 1 3
+8 1
+-2 -3 4 -1 -2 1 5 -3
+1 7
 
 Output:
-7
-15
-
-Explanation:
-Testcase 1: 3rd smallest element in the given array is 7.
-
+4 5
+4
  */
-public class KthSmallestElementInArray {
+public class MaxPrefixSumForRange {
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
+		int tests = sc.nextInt();
 
-		while (t-- > 0) {
+		while (tests-- > 0) {
+			System.out.println("s1");
+
 			int n = sc.nextInt();
+			int queries = sc.nextInt();
+			//System.out.println("size:" + size+", queries:"+queries)
 			int arr[] = new int[n];
-
 			for (int i = 0; i < n; i++) {
 				arr[i] = sc.nextInt();
 			}
+			System.out.println("s2");
 
-			int k = sc.nextInt();
-			System.out.println("Input: " + Arrays.toString(arr) +" Kth index: "+ k);
-			System.out.println(kthsmallest(arr, 0, n - 1, k));
+			//System.out.println("Array = " + Arrays.toString(arr));
+
+			//System.out.println("Ranges = " + Arrays.toString(qs));
+			for(int i=0; i<queries*2; i=i+2){
+				System.out.println("s3.1 i="+i);
+				int r1 = sc.nextInt();
+				int r2 = sc.nextInt();
+				System.out.println("s3.2 i="+i);
+				int sum = findPrefixSum(arr,r1, r2);
+				System.out.println("s3.3 i="+i);
+				System.out.print(sum + " ");
+			}
+			System.out.println("done");
 		}
 	}
-	static int kthsmallest(int arr[], int startIndex, int lastIndex, int k) {
-		// If k is smaller than number of elements in array
-		if (k > 0 && k <= lastIndex - startIndex + 1) {
-
-			System.out.println("call randomPartition: arr:" + Arrays.toString(Arrays.copyOfRange(arr, startIndex, lastIndex+1))
-							   + " startIndex:"+ startIndex + " lastIndex:" +lastIndex);
-			// partitioning the array along the pivot
-			int pos = randomPartition(arr, startIndex, lastIndex);
-
-			System.out.println("Position received: "+ pos
-			 +" findal postion if (pos-startIndex==k-1)="+"("+pos
-			+"-"+startIndex+"=="+k+"-"+1+")=>"+(pos - startIndex == k - 1));
-			// check if current element gives you the kth smallest element
-			if (pos - startIndex == k - 1) {
-				return arr[pos];
-			}
-
-			System.out.println("Recurse on left if true otherwise on right \n"
-							   + " (pos - startIndex > k - 1)="
-							   +"("+pos +"-"+startIndex+">"+k+"-"+1+")=>" + (pos - startIndex > k - 1));
-			// else recurse for the left and right half accordingly
-			if (pos - startIndex > k - 1) {
-				return kthsmallest(arr, startIndex, pos - 1, k);
-			}
-			return kthsmallest(arr, pos + 1, lastIndex, k - pos + startIndex - 1);
+	static int findPrefixSum(int arr[], int startIndex, int lastIndex) {
+		int sum = 0;
+		for(int i = startIndex; i<lastIndex; i++){
+			sum += arr[i];
 		}
-
-		return Integer.MAX_VALUE;
+		return sum;
 	}
-
-	static int partition(int arr[], int startIndex, int lastIndex) {
-		System.out.println("\t start Partition arr: "
-						   + Arrays.toString(Arrays.copyOfRange(arr, startIndex, lastIndex+1))
-						   + " startIndex:"+ startIndex +" lastIndex:"+lastIndex
-							+" x="+ arr[lastIndex]);
-		int x = arr[lastIndex], i = startIndex;
-		for (int j = startIndex; j <= lastIndex - 1; j++) {
-			if (arr[j] <= x) {
-				System.out.println("\t in Partition, swapping if (arr[j] <= x)= "
-								   + "("+arr[j]+"<= "+x+") => "+(arr[j] <= x));
-				int temp = arr[j];
-				arr[j] = arr[i];
-				arr[i] = temp;
-				i++;
-				System.out.println("\t In Partition, after swap arr: "+ Arrays.toString(Arrays.copyOfRange(arr, startIndex, lastIndex+1)));
-			}else{
-				System.out.println("\t in Partition, No swapping if (arr[j] <= x)= "
-								   + "("+arr[j]+"<= "+x+") => "+(arr[j] <= x));
-			}
-		}
-		System.out.println("\t in Partition, swap lastIndex(last swapped index) with ith "
-						   + "arr["+lastIndex+"] with arr["+i+"] => "
-						   + arr[lastIndex]+" with " + arr[i]);
-		int temp = arr[lastIndex];
-		arr[lastIndex] = arr[i];
-		arr[i] = temp;
-		System.out.println("\t After partition arr: "+ Arrays.toString(Arrays.copyOfRange(arr, startIndex, lastIndex+1))
-						   + " partition index:"+ i);
-		return i;
-	}
-
-	// Function to partition the array along the random pivot
-	static int randomPartition(int arr[], int startIndex, int lastIndex) {
-		Random rand = new Random();
-		int n = lastIndex - startIndex + 1;
-		// approach 1, use random number as pivot and then swap with last number
-		int pivot = rand.nextInt(n);
-		System.out.println("in randomPartition, pivot: "+ pivot);
-		int temp = arr[lastIndex];
-		arr[lastIndex] = arr[startIndex + pivot];
-		arr[startIndex + pivot] = temp;
-		// approach 2, use last index as pivot and no swap. but takes 50% time more than approach 1
-		// int pivot = lastIndex;
-		System.out.println("in randomPartition, updated arr: "+ Arrays.toString(Arrays.copyOfRange(arr, startIndex, lastIndex+1))
-						   + " startIndex:"+startIndex+" lastIndex:"+lastIndex);
-		return partition(arr, startIndex, lastIndex);
-	}
-
-
 }
