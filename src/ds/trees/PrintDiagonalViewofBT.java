c1265458ab8194381e6ba67de16b541cf491ba6a
Sandeep Gupta <sandeep.apjjal@gmail.com>
2026-09-26 11:41:52 +0530

    Update PrintDiagonalViewofBT.java

--- src/ds/trees/PrintDiagonalViewofBT.java
+++ src/ds/trees/PrintDiagonalViewofBT.java
@@ -1,71 +1,86 @@
 package ds.trees;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class PrintDiagonalViewofBT {
-	//https://www.techiedelight.com/print-diagonal-traversal-binary-tree/
-	public void diagonalPrint(Node root)
-	{
-		//add your code here.
-		StringBuilder sb = new StringBuilder();
-		Queue<Node> queue = new LinkedList<>();
-		queue.add(root);
-		while (!queue.isEmpty()){
-			Node node =queue.remove();
-			diagonalPrint(node, queue, sb);
-		}
-		System.out.print(sb.toString().trim());
+/*
+* https://www.techiedelight.com/print-diagonal-traversal-binary-tree/
+*/
+// A class to store a binary tree node
+class Node
+{
+	int data;
+	Node left = null, right = null;
+
+	Node(int data) {
+		this.data = data;
 	}
-	public void diagonalPrint(Node root,Queue<Node> queue, StringBuilder sb){
-		if(root == null){
+}
+
+class Main
+{
+	// Recursive function to perform preorder traversal on the tree and
+	// fill the map with diagonal elements
+	public static void printDiagonal(Node node, int diagonal,
+									Map<Integer, List<Integer>> map)
+	{
+		// base case: empty tree
+		if (node == null) {
 			return;
 		}
-		sb.append(root.data);
-		sb.append(" ");
-		if(root.left != null){
-			queue.add(root.left);
-		}
-		diagonalPrint(root.right, queue, sb);
-	}
-	
 
-	public static void main(String[] args) {
+		// insert the current node into the current diagonal
+		map.putIfAbsent(diagonal, new ArrayList<>());
+		map.get(diagonal).add(node.data);
+
+		// recur for the left subtree by increasing diagonal by 1
+		printDiagonal(node.left, diagonal + 1, map);
+
+		// recur for the right subtree with the same diagonal
+		printDiagonal(node.right, diagonal, map);
+	}
+
+	// Function to print the diagonal elements of a given binary tree
+	public static void printDiagonal(Node root)
+	{
+		// create an empty multimap to store the diagonal element in every slope
+		Map<Integer, List<Integer>> map = new HashMap<>();
+
+		// perform preorder traversal on the tree and fill the map
+		printDiagonal(root, 0, map);
+
+		// traverse the map and print the diagonal elements
+		for (int i = 0; i < map.size(); i++) {
+			System.out.println(map.get(i));
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		/* Construct the following tree
+				  1
+				/   \
+			   /     \
+			  2       3
+			 /      /  \
+			/      /    \
+		   4      5      6
+				 / \
+				/   \
+			   7     8
+		*/
+
 		Node root = new Node(1);
 		root.left = new Node(2);
 		root.right = new Node(3);
 		root.left.left = new Node(4);
-		root.left.left.left = new Node(8);
-		root.left.right = new Node(5);
-		root.right.right = new Node(7);
-		root.right.left = new Node(11);
+		root.right.left = new Node(5);
+		root.right.right = new Node(6);
+		root.right.left.left = new Node(7);
+		root.right.left.right = new Node(8);
 
-		/*
-				7
-			3		
-				11
-		1
-				5
-			2
-				4
-					8
-					
-		8	4	2	1,5,11		3	7		
-		*/
-		PrintDiagonalViewofBT p = new PrintDiagonalViewofBT();
-		p.diagonalPrint(root);
+		printDiagonal(root);
 	}
-	
-	static class Node {
-		int data;
-		Node left;
-		Node right;
-
-		public Node(int data) {
-			this.data = data;
-			left = null;
-			right = null;
-		}
-	}
-
 }
