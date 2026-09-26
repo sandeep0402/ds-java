a3c89e3d63249fda59ec6c4ba2559c53c14ae77f
Sandeep Gupta <sandeep.apjjal@gmail.com>
2026-09-26 17:40:14 +0530

    Create PrintBinaryTreeMirror.java

---
+++ src/ds/trees/PrintBinaryTreeMirror.java
@@ -0,0 +1,29 @@
+package ds.trees.advanced;
+
+/** https://leetcode.com/problems/invert-binary-tree/
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class PrintBinaryTreeMirror {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        TreeNode temp = root.left;
+        root.left= invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
+}
+
